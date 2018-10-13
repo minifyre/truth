3d86b06c1b5d3b353e12e509834e7448404db3b1
Minifyre <minifyre@gmail.com>
2018-10-13 10:56:42 -0500

    +truth.y()
    truth.y() will return an object with more properties, giving the developer more fine-grained control over the lifescycle of proxy traps, while still maintaining compatibility with prior uses of truth that just return the proxy (will reevaluate in future if truth should return truth.y values by default of if they should be kept separate for ease of use).

--- truth.mjs
+++ truth.mjs
@@ -1,16 +1,6 @@
 export default function truth(...ops)
 {
-	let
-	i=ops.findIndex(x=>!(x instanceof Function)),
-	[pre,state,post]=truth.zipList(ops,i),
-	send=function(act)//promise return prevents holding up subsequent code
-	{
-		act=truth.compose(pre,act)
-		return act?new Promise(res=>res(truth.compose(post,act))):act
-	}
-	pre.push(act=>truth.inject(state,act))
-	send({type:'set',path:[],val:state})
-	return truth.proxy(send,state)
+	return truth.y(...ops)
 }
 truth.compose=(fns,arg)=>fns.reduce((arg,fn)=>fn(arg),arg)
 truth.inject=function(state,act)
@@ -36,4 +26,18 @@ truth.proxy=function(send,obj,path=[])
 	}):obj
 }
 truth.ref=(ref,path)=>path.reduce((ref,prop)=>ref[prop],ref)
+truth.y=function(...ops)
+{
+	let
+	i=ops.findIndex(x=>!(x instanceof Function)),
+	[pre,state,post]=truth.zipList(ops,i),
+	send=function(act)//promise return prevents holding up subsequent code
+	{
+		act=truth.compose(pre,act)
+		return act?new Promise(res=>res(truth.compose(post,act))):act
+	}
+	pre.push(act=>truth.inject(state,act))
+	send({type:'set',path:[],val:state})
+	return truth.proxy(send,state)
+}
 truth.zipList=(x,i)=>[x.slice(0,i),x[i],x.slice(i+1)]
